a33d74669c087ec87e729c3b0c0d72ecca3843c5
Meadowsys <blazeykirin@gmail.com>
2023-02-16 23:12:46 -0800

    add a comment

--- lib/src/runtime_meta/action.rs
+++ lib/src/runtime_meta/action.rs
@@ -2,10 +2,12 @@ use crate::error::Result;
 pub enum Action {
 	CopyFile {
 		from: String,
+		/// this is relative to the base of the built resource pack
 		to: String
 	},
 	WriteBytes {
 		data: Vec<u8>,
+		/// is is relative to the base of the built resource pack
 		path: String,
 		/// vec of paths of files that caused the compilation of this entry,
 		/// the "source" if you will
