1131058835e98638fbc4c5ef8baeef22f463d286
Meadowsys <blazeykirin@gmail.com>
2023-02-22 00:32:10 -0800

    scrap that we hardcode

--- lib/src/meta/pack_version_specifier.rs
+++ lib/src/meta/pack_version_specifier.rs
@@ -4,7 +4,14 @@ use crate::error::{ Error, Result };
 use crate::util::RON;
 use serde::{ Deserialize, Serialize };
 
-pub const MC_VERSION_MANIFEST_URL: &str = "https://launchermeta.mojang.com/mc/game/version_manifest.json";
+#[derive(Clone, Debug)]
+pub struct PackVersion {
+	name: &'static str,
+	release_type: MCVersionType,
+	format: Option<u8>
+}
+
+const PACK_FORMATS: &[PackVersion] = include!("./pack_formats");
 
 #[derive(Debug, Deserialize, Serialize)]
 pub enum PackVersionSpecifier {
@@ -13,62 +20,33 @@ pub enum PackVersionSpecifier {
 	MCVersionRange(String, String)
 }
 
-impl PackVersionSpecifier {
-	pub async fn fetch_mc_versions() -> Result<MCReleases> {
-		let client = reqwest::ClientBuilder::new()
-			.brotli(true)
-			.gzip(true)
-			.https_only(true)
-			.use_rustls_tls()
-			.no_trust_dns()
-			.build()
-			.map_err(|e| Error::UnableToInitialiseHttpClient { source: e })?;
-
-		let fetched = client.get(MC_VERSION_MANIFEST_URL)
-			.send()
-			.await
-			.map_err(|e| Error::FailedToFetchMCVersions { source: e })?
-			.bytes()
-			.await
-			.map_err(|e| Error::FailedToFetchMCVersions { source: e })?
-			.into_iter()
-			.collect::<Vec<_>>();
-		let fetched = String::from_utf8(fetched)
-			.map_err(|e| Error::FailedToFetchMCVersionsInvalidUTF8 { source: e })?;
-
-		let parsed = RON.from_str::<MCReleases>(&fetched)
-			.map_err(|e| Error::ParseErrorRonSpannedError { path: "<http response>".into(), source: e })?;
-
-		Ok(parsed)
-	}
-}
-
+/// Responses to release list endpoint from Mojang
 #[derive(Debug, Deserialize, Serialize)]
 pub struct MCReleases {
-	latest: MCLatestRelease,
-	versions: Vec<MCVersion>
+	pub latest: MCLatestRelease,
+	pub versions: Vec<MCVersion>
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct MCLatestRelease {
-	release: String,
-	snapshot: String
+	pub release: String,
+	pub snapshot: String
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct MCVersion {
-	id: String,
+	pub id: String,
 	#[serde(rename = "type")]
-	version_type: MCVersionType,
+	pub version_type: MCVersionType,
 	#[serde(rename = "url")]
-	manifest_url: String,
+	pub manifest_url: String,
 	#[serde(rename = "time")]
-	time_generated_by_server_or_something: DateTime<UTC>,
+	pub time_generated_by_server_or_something: DateTime<UTC>,
 	#[serde(rename = "releaseTime")]
-	release_time: DateTime<UTC>
+	pub release_time: DateTime<UTC>
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub enum MCVersionType {
 	#[serde(rename = "release")]
 	Release,
