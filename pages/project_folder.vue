56e6a136f71f8dcd0b57424f5375c646a53f9bf0
Meadowsys <blazeykirin@gmail.com>
2023-03-05 18:22:51 -0800

    format the project window and get inner scroll working

--- pages/project_folder.vue
+++ pages/project_folder.vue
@@ -1,6 +1,77 @@
 <template>
-	<div>
-		path/label: {{ path }}
+	<div class="flex flex-col h-full">
+		<div class="h-[28px] bg-white text-center select-none cursor-default" data-tauri-drag-region>
+			Project: {{ path }}
+		</div>
+		<div class="flex-grow overflow-scroll">
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+			path: {{ path }}<br>
+		</div>
 	</div>
 </template>
 
