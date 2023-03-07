33523dedf052805c6f0cc9e7f544ba1f7a2de1f8
Meadowsys <blazeykirin@gmail.com>
2023-03-07 12:45:20 -0800

    fiiiiiiiiiiiiiiine i'll authenticate

--- scripts/update-tauri-version-to-next.ts
+++ scripts/update-tauri-version-to-next.ts
@@ -5,14 +5,15 @@ import {
 import {
 	resolve as resolve_path
 } from "path";
-import { owner, repo, get_new_tag_name, get_gh } from "./lib";
+import { owner, repo, get_new_tag_name, get_env, get_gh } from "./lib";
 
 export const tag_name = (async () => {
 	let tauri_manifest_path = resolve_path("./src-tauri/tauri.conf.json");
 	let tauri_manifest_obj = JSON.parse(read_file(tauri_manifest_path, "utf-8"));
 	let version = tauri_manifest_obj.package.version as string;
+	let gh_public_pat = get_env("github_public_pat").get_optional() || undefined;
 
-	let gh = get_gh("update-tauri-version-to-next script");
+	let gh = get_gh("update-tauri-version-to-next script", gh_public_pat);
 
 	let [latest, tag_name] = await get_new_tag_name(gh, owner, repo, version);
 
