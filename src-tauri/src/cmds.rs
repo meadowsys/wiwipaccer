ccf95b00c7cefadce3a2b271ceeb819effe619e2
Meadowsys <blazeykirin@gmail.com>
2023-03-10 01:37:21 -0800

    misplaced copypasted comment lol
    from when this was copypasted out into its own function, i guess i didn't check that comment lol (it built that was good enough)

--- src-tauri/src/cmds.rs
+++ src-tauri/src/cmds.rs
@@ -80,6 +80,8 @@ pub async fn open_project<R: Runtime>(app: AppHandle<R>) {
 					window.set_focus()
 						.expect("couldn't focus the window");
 				} else {
+					// TODO maybe send a signal back to main or something if this is Err
+					// so that user gets an alert that opening it failed
 					let window = get_window_builder(&app, &label, WindowUrl::App("project_folder".into()))
 						.inner_size(800., 500.)
 						.min_inner_size(800., 500.)
@@ -103,8 +105,6 @@ fn get_window_builder<'h, R: Runtime>(app: &'h AppHandle<R>, label: &'h str, url
 	#[cfg(target_os = "macos")]
 	let builder = builder.title_bar_style(TitleBarStyle::Overlay);
 
-	// TODO send a signal back to main or something if this is Err
-	// so that user gets an alert that opening it failed
 	builder
 }
 
