11d9d4ce308db4a9c41028d099d40edc7e0d850b
Meadowsys <blazeykirin@gmail.com>
2023-04-12 14:55:53 -0700

    update base, keep a cfg if exists

--- base.nuxt.config.ts
+++ base.nuxt.config.ts
@@ -6,12 +6,12 @@ export function extend_base(cfg: NuxtConfig) {
 	cfg.telemetry = false;
 
 	cfg.devServer = cfg.devServer || {};
-	cfg.devServer.port = 3000;
+	cfg.devServer.port = cfg.devServer.port || 3000;
 
 	cfg.postcss = cfg.postcss || {};
 	cfg.postcss.plugins = cfg.postcss.plugins || {};
-	cfg.postcss.plugins.tailwindcss = {};
-	cfg.postcss.plugins.autoprefixer = {};
+	cfg.postcss.plugins.tailwindcss = cfg.postcss.plugins.tailwindcss || {};
+	cfg.postcss.plugins.autoprefixer = cfg.postcss.plugins.autoprefixer || {};
 
 	return cfg;
 }
