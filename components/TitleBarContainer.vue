f5025804f5886f36800f0626cf2a83175d58c135
Meadowsys <blazeykirin@gmail.com>
2023-04-13 18:25:20 -0700

    seperate out title bar into its own container component

---
+++ components/TitleBarContainer.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="flex flex-col h-full">
+		<div class="h-[6rem] bg-white text-center select-none cursor-default" data-tauri-drag-region>
+			{{ title }}
+		</div>
+		<div class="flex-grow overflow-scroll">
+			<slot />
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+	defineProps<{
+		title: string;
+	}>();
+</script>
