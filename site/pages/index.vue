3c30ad3595bab8325e633f0e0cfaa65195188e63
Meadowsys <blazeykirin@gmail.com>
2023-04-14 11:29:21 -0700

    add a tiny bit of actual content to the site h

--- site/pages/index.vue
+++ site/pages/index.vue
@@ -1,5 +1,23 @@
 <template>
-	<div class="bg-purple-400">
-		hhhhhhhhhalllo
+	<div class="flex flex-col h-full">
+		<div class="flex-grow" />
+
+		<div class="flex flex-row">
+			<div class="flex-grow" />
+
+			<div>
+				<div class="font-permanent-marker text-6xl text-center">
+					wiwipaccer
+				</div>
+				<br>
+				<div class="text-center">
+					coming soon&trade;
+				</div>
+			</div>
+
+			<div class="flex-grow" />
+		</div>
+
+		<div class="flex-grow" />
 	</div>
 </template>
