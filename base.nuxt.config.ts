df18344ec0b61ec6446bfa6f16c47606906a2ad1
Meadowsys <blazeykirin@gmail.com>
2023-04-13 19:46:16 -0700

    oh yeah this operator exists lol

--- base.nuxt.config.ts
+++ base.nuxt.config.ts
@@ -5,13 +5,13 @@ import { NuxtConfig } from "@nuxt/schema";
 export function extend_base(cfg: NuxtConfig) {
 	cfg.telemetry = false;
 
-	cfg.devServer = cfg.devServer || {};
-	cfg.devServer.port = cfg.devServer.port || 3000;
+	cfg.devServer ??= {};
+	cfg.devServer.port ??= 3000;
 
-	cfg.postcss = cfg.postcss || {};
-	cfg.postcss.plugins = cfg.postcss.plugins || {};
-	cfg.postcss.plugins.tailwindcss = cfg.postcss.plugins.tailwindcss || {};
-	cfg.postcss.plugins.autoprefixer = cfg.postcss.plugins.autoprefixer || {};
+	cfg.postcss ??= {};
+	cfg.postcss.plugins ??= {};
+	cfg.postcss.plugins.tailwindcss ??= {};
+	cfg.postcss.plugins.autoprefixer ??= {};
 
 	return cfg;
 }
