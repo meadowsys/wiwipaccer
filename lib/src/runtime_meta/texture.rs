b218695b04d7f04c855388b76700b6a72de6c5f9
Meadowsys <blazeykirin@gmail.com>
2023-04-19 16:24:48 -0700

    seperate texture into mc-versioned and non-mc-versioned

--- lib/src/runtime_meta/texture.rs
+++ lib/src/runtime_meta/texture.rs
@@ -2,7 +2,7 @@ use ahash::{ RandomState, HashMapExt };
 use crate::error::{ Error, Result };
 use crate::meta::texture::Texture;
 use crate::runtime_meta::pack_version_specifier::PackVersionSpecifierRuntimeMeta;
-use crate::runtime_meta::option::{ OptionRuntimeMeta, AvailableOptionRuntimeMeta, UnavailableOptionRuntimeMeta };
+use crate::runtime_meta::option;
 use crate::runtime_meta::{ Message, MessageSeverity };
 use crate::util::RON;
 use std::collections::HashMap;
@@ -10,15 +10,28 @@ use super::META_NAME;
 use tokio::fs;
 
 #[derive(Debug)]
-pub enum TextureRuntimeMeta {
-	Available(AvailableTextureRuntimeMeta),
-	Unavailable(UnavailableTextureRuntimeMeta)
+pub struct WithoutMCVersion(InnerWithoutMCVersion);
+
+#[derive(Debug)]
+pub struct InnerWithoutMCVersion {
+	pub path: String,
+	pub shortpath: String,
+	pub name: String,
+	pub description: String,
+	pub options: HashMap<String, option::WithoutMCVersion, RandomState>,
+	pub messages: Vec<Message>
 }
 
 #[derive(Debug)]
-pub struct AvailableTextureRuntimeMeta(InnerAvailable);
+pub enum WithMCVersion {
+	Available(Available),
+	Unavailable(Unavailable)
+}
+
 #[derive(Debug)]
-pub struct UnavailableTextureRuntimeMeta(InnerUnavailable);
+pub struct Available(InnerAvailable);
+#[derive(Debug)]
+pub struct Unavailable(InnerUnavailable);
 
 #[derive(Debug)]
 pub struct InnerAvailable {
@@ -27,8 +40,8 @@ pub struct InnerAvailable {
 	pub name: String,
 	pub description: String,
 	pub default: Option<String>,
-	pub available_options: HashMap<String, AvailableOptionRuntimeMeta, RandomState>,
-	pub unavailable_options: HashMap<String, UnavailableOptionRuntimeMeta, RandomState>,
+	pub available_options: HashMap<String, option::Available, RandomState>,
+	pub unavailable_options: HashMap<String, option::Unavailable, RandomState>,
 	pub messages: Vec<Message>
 }
 
@@ -38,15 +51,16 @@ pub struct InnerUnavailable {
 	pub shortpath: String,
 	pub name: String,
 	pub description: String,
-	pub options: HashMap<String, OptionRuntimeMeta, RandomState>,
+	pub options: HashMap<String, option::WithMCVersion, RandomState>,
 	pub messages: Vec<Message>
 }
 
-crate::impl_deref!(AvailableTextureRuntimeMeta, target InnerAvailable);
-crate::impl_deref!(UnavailableTextureRuntimeMeta, target InnerUnavailable);
+crate::impl_deref!(WithoutMCVersion, target InnerWithoutMCVersion);
+crate::impl_deref!(Available, target InnerAvailable);
+crate::impl_deref!(Unavailable, target InnerUnavailable);
 
-impl TextureRuntimeMeta {
-	pub async fn new(path: &str, mc_version: PackVersionSpecifierRuntimeMeta) -> Result<Self> {
+impl WithoutMCVersion {
+	pub async fn new(path: &str) -> Result<Self> {
 		let mut messages = vec![];
 		let manifest_path = format!("{path}/{META_NAME}");
 
@@ -78,9 +92,9 @@ impl TextureRuntimeMeta {
 			}
 		};
 
-		let mut default = vec![];
-		let mut available_options: HashMap<String, AvailableOptionRuntimeMeta, RandomState> = HashMapExt::new();
-		let mut unavailable_options: HashMap<String, UnavailableOptionRuntimeMeta, RandomState> = HashMapExt::new();
+		// let mut available_options: HashMap<String, AvailableOptionRuntimeMeta, RandomState> = HashMapExt::new();
+		// let mut unavailable_options: HashMap<String, UnavailableOptionRuntimeMeta, RandomState> = HashMapExt::new();
+		let mut options: HashMap<String, option::WithoutMCVersion, RandomState> = HashMapExt::new();
 
 		let mut dir_contents = fs::read_dir(&path).await
 			.map_err(|e| Error::IOError { source: e })?;
@@ -102,19 +116,12 @@ impl TextureRuntimeMeta {
 				continue
 			}
 
-			match OptionRuntimeMeta::new(dir_entry_path, mc_version.clone()).await {
-				Ok(option) => match option {
-					OptionRuntimeMeta::Available(option) => {
-						if option.default { default.push(option.shortpath.clone()) }
-						available_options.insert(option.shortpath.clone(), option);
-					}
-					OptionRuntimeMeta::Unavailable(option) => {
-						unavailable_options.insert(option.shortpath.clone(), option);
-					}
+			match option::WithoutMCVersion::new(dir_entry_path).await {
+				Ok(option) => {
+					// TODO handle defaults
+					options.insert(option.shortpath.clone(), option);
 				}
-				Err(err) => {
-					messages.push(err.to_message());
-				}
+				Err(e) => { messages.push(e.to_message()) }
 			}
 		}
 
@@ -125,43 +132,88 @@ impl TextureRuntimeMeta {
 			.unwrap()
 			.into();
 
-		if available_options.is_empty() {
+		Ok(WithoutMCVersion(InnerWithoutMCVersion {
+			path: path.into(),
+			shortpath,
+			name,
+			description,
+			options,
+			messages
+		}))
+	}
+}
+
+impl WithMCVersion {
+	pub async fn from(
+		texture_without_mc_version: &WithoutMCVersion,
+		mc_version: PackVersionSpecifierRuntimeMeta
+	) -> Result<Self> {
+		if texture_without_mc_version.options.is_empty() {
+			return Ok(WithMCVersion::Unavailable(Unavailable(InnerUnavailable {
+				path: texture_without_mc_version.path.clone(),
+				shortpath: texture_without_mc_version.shortpath.clone(),
+				name: texture_without_mc_version.name.clone(),
+				description: texture_without_mc_version.description.clone(),
+				options: HashMapExt::new(),
+				messages: texture_without_mc_version.messages.clone()
+			})))
+		}
+
+		let mut messages = texture_without_mc_version.messages.clone();
+		let mut default = vec![]; // TODO change this
+		let mut available = HashMap::<String, option::Available, RandomState>::new();
+		let mut unavailable = HashMap::<String, option::Unavailable, RandomState>::new();
+
+		for (shortpath, option) in texture_without_mc_version.options.iter() {
+			use option::WithMCVersion::*;
+			match option::WithMCVersion::from(option, mc_version.clone()).await {
+				Ok(option) => match option {
+					Available(option) => {
+						if option.default { default.push(option.shortpath.clone()) }
+						available.insert(shortpath.clone(), option);
+					}
+					Unavailable(option) => {
+						unavailable.insert(shortpath.clone(), option);
+					}
+				}
+				Err(err) => {
+					messages.push(err.to_message());
+				}
+			}
+		}
+
+		if available.is_empty() {
 			messages.push(Error::UnavailableInfo {
-				thing: format!("Texture {shortpath}"),
+				thing: format!("Texture {}", texture_without_mc_version.shortpath),
 				reason: "no options are available".into()
 			}.to_message());
 
-			return Ok(TextureRuntimeMeta::Unavailable(UnavailableTextureRuntimeMeta(InnerUnavailable {
-				path: path.into(),
-				shortpath,
-				name,
-				description,
-				options: {
-					unavailable_options.into_iter()
-						.map(|(shortpath, o)| (shortpath, OptionRuntimeMeta::Unavailable(o)))
-						.collect()
-				},
+			return Ok(Self::Unavailable(Unavailable(InnerUnavailable {
+				path: texture_without_mc_version.path.clone(),
+				shortpath: texture_without_mc_version.shortpath.clone(),
+				name: texture_without_mc_version.name.clone(),
+				description: texture_without_mc_version.description.clone(),
+				options: unavailable.into_iter().map(|(s, o)| (s, option::WithMCVersion::Unavailable(o))).collect(),
 				messages
 			})))
 		}
 
 		if default.len() > 1 {
 			messages.push(Error::UnavailableError {
-				thing: format!("Texture {shortpath}"),
+				thing: format!("Texture {}", texture_without_mc_version.shortpath),
 				reason: "more than 1 default was specified".into()
 			}.to_message());
 
-			return Ok(TextureRuntimeMeta::Unavailable(UnavailableTextureRuntimeMeta(InnerUnavailable {
-				path: path.into(),
-				shortpath,
-				name,
-				description,
+			return Ok(Self::Unavailable(Unavailable(InnerUnavailable {
+				path: texture_without_mc_version.path.clone(),
+				shortpath: texture_without_mc_version.shortpath.clone(),
+				name: texture_without_mc_version.name.clone(),
+				description: texture_without_mc_version.description.clone(),
 				options: {
-					let unavailable_iter = unavailable_options.into_iter()
-						.map(|(shortpath, o)| (shortpath, OptionRuntimeMeta::Unavailable(o)));
-
-					available_options.into_iter()
-						.map(|(shortpath, o)| (shortpath, OptionRuntimeMeta::Available(o)))
+					let unavailable_iter = unavailable.into_iter()
+						.map(|(shortpath, o)| (shortpath, option::WithMCVersion::Unavailable(o)));
+					available.into_iter()
+						.map(|(shortpath, o)| (shortpath, option::WithMCVersion::Available(o)))
 						.chain(unavailable_iter)
 						.collect()
 				},
@@ -169,20 +221,21 @@ impl TextureRuntimeMeta {
 			})))
 		}
 
+		// TODO this needs to be moved to be declared in self instead of in the options
 		let default = if default.is_empty() {
 			None
 		} else {
 			Some(default.into_iter().next().unwrap())
 		};
 
-		Ok(TextureRuntimeMeta::Available(AvailableTextureRuntimeMeta(InnerAvailable {
-			path: path.into(),
-			shortpath,
-			name,
-			description,
+		Ok(WithMCVersion::Available(Available(InnerAvailable {
+			path: texture_without_mc_version.path.clone(),
+			shortpath: texture_without_mc_version.shortpath.clone(),
+			name: texture_without_mc_version.name.clone(),
+			description: texture_without_mc_version.description.clone(),
 			default,
-			available_options,
-			unavailable_options,
+			available_options: available,
+			unavailable_options: unavailable,
 			messages
 		})))
 	}
