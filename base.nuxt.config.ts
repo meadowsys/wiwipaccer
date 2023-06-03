67459a99ac618bba279add31c13863743f21f482
Meadowsys <blazeykirin@gmail.com>
2023-06-03 01:46:13 -0700

    use terser and compress 3 times?

--- base.nuxt.config.ts
+++ base.nuxt.config.ts
@@ -16,10 +16,23 @@ export function extend_base(cfg: NuxtConfig) {
 	// cfg.modules ??= [];
 	// cfg.modules.push("i18n");
 
-	cfg.typescript ??= {}
+	cfg.typescript ??= {};
 	cfg.typescript.shim ??= false;
 	cfg.typescript.strict ??= true;
 	cfg.typescript.typeCheck ??= "build";
+
+	cfg.vite ??= {};
+	cfg.vite.build ??= {};
+	cfg.vite.build.minify = "terser";
+	cfg.vite.build.terserOptions ??= {};
+	// assuming this will not be false
+	if (typeof cfg.vite.build.terserOptions.compress !== "undefined") {
+		if (typeof cfg.vite.build.terserOptions.compress === "object") {
+			cfg.vite.build.terserOptions.compress.passes ??= 3;
+		}
+	} else {
+		cfg.vite.build.terserOptions.compress = { passes: 3 };
+	}
 
 	return cfg;
 }
