cc866a4fd7783f294f7b75368ab0a793b974ff35
Meadowsys <blazeykirin@gmail.com>
2023-06-20 23:10:54 +0200

    updated title bar container to have a right slot

--- components/TitleBarContainer.vue
+++ components/TitleBarContainer.vue
@@ -1,9 +1,15 @@
 <template>
 	<div class="flex flex-col h-full">
-		<div class="h-[26px] bg-white text-center select-none cursor-default flex-shrink-0 flex-grow-0" data-tauri-drag-region>
-			{{ title }}
+		<div class="h-[26px] bg-white select-none cursor-default flex-shrink-0 flex-grow-0 flex flex-row">
+			<div class="flex-grow" data-tauri-drag-region />
+			<div data-tauri-drag-region>
+				{{ title }}
+			</div>
+			<div class="flex-grow basis-0" data-tauri-drag-region>
+				<slot name="right" />
+			</div>
 		</div>
-		<div class="mx-10 border border-neutral-200" />
+		<div class="border border-neutral-200" />
 		<div class="flex-grow overflow-scroll">
 			<slot />
 		</div>
