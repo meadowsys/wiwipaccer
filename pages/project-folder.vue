a0c93800f024ad7f8f8220678bdf8a5356f7d387
Meadowsys <blazeykirin@gmail.com>
2023-06-21 19:23:41 +0200

    add success/error states, add version dropdown UI

--- pages/project-folder.vue
+++ pages/project-folder.vue
@@ -6,6 +6,13 @@
 		<template #right>
 			<div class="flex flex-row w-full h-full" data-tauri-drag-region>
 				<div class="flex-grow" data-tauri-drag-region />
+				<div v-if="state.state === 'success'" class="flex flex-col">
+					<div class="flex-grow" data-tauri-drag-region />
+					<select class="select select-bordered select-xs mr-2 focus:outline-none hover:border-primary cursor-default">
+						<option v-for="version in state.project.versions">{{ version.name }}</option>
+					</select>
+					<div class="flex-grow" data-tauri-drag-region />
+				</div>
 				<div class="flex flex-col">
 					<div class="flex-grow" data-tauri-drag-region />
 					<select class="select select-bordered select-xs mr-2 focus:outline-none hover:border-primary cursor-default">
@@ -16,27 +23,21 @@
 				</div>
 			</div>
 		</template>
+		<template v-if="state.state === 'loading'">
+			loading...
+		</template>
+		<template v-else-if="state.state === 'success'">
+		</template>
+		<template v-else-if="state.state === 'error'">
+			error: {{ state.e }}
+		</template>
 	</title-bar-container>
 </template>
 
 <script setup lang="ts">
 	import { appWindow } from "@tauri-apps/api/window";
-	let path = await invoke_decode_hex_string(appWindow.label);
+	import type { ProjectSupportedVersions } from "~/composables/project-meta"
 
-	type Loading = {
-		state: "loading";
-	};
-
-	type State = Loading;
-
-	let title = ref("Loading...");
-	let state = ref<State>({ state: "loading" });
-
-	invoke_add_recent_project(path);
-</script>
-
-<!-- <script setup lang="ts">
-	import { appWindow } from "@tauri-apps/api/window";
 	let path = await invoke_decode_hex_string(appWindow.label);
 
 	type Loading = {
@@ -45,7 +46,7 @@
 
 	type Success = {
 		state: "success";
-		supported_versions: Awaited<ReturnType<typeof invoke_get_project_supported_versions>>;
+		project: ProjectSupportedVersions;
 	};
 
 	type Error = {
@@ -53,25 +54,28 @@
 		e: any;
 	};
 
-	let things = ref<Loading | Success | Error>({ state: "loading" });
-	let title = computed(() => {
-		if (things.value.state === "loading") return "Loading...";
+	type State = Loading | Success | Error;
 
-		if (things.value.state === "success") {
-			let names = things.value.supported_versions.names
+	let state = ref<State>({ state: "loading" });
+	let title = computed(() => {
+		if (state.value.state === "loading") return "Loading...";
+
+		if (state.value.state === "success") {
+			let names = state.value.project.names
 			if (names.length === 1) return names[0];
 			return `Workspace: ${names.join(", ")}`;
 		}
 
-		if (things.value.state === "error") return "Error in loading workspace";
+		if (state.value.state === "error") return "Error in loading workspace";
 		throw "this should never happen lol";
 	});
-	invoke_get_project_supported_versions(path)
-		.then(supported_versions => {
-			things.value = { state: "success", supported_versions };
+
+	invoke_get_project_basic_meta(path)
+		.then(project => {
+			state.value = { state: "success", project };
 			invoke_add_recent_project(path);
 		})
 		.catch(e => {
-			things.value = { state: "error", e };
+			state.value = { state: "error", e };
 		});
-</script> -->
+</script>
