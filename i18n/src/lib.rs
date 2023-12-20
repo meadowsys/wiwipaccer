25df308181b7396a050307eb1fe88321e2d06ca0
Meadowsys <blazeykirin@gmail.com>
2023-12-19 22:19:23 -0800

    after a long break... wtf was i doing here lets start over

--- i18n/src/lib.rs
+++
@@ -1,5 +0,0 @@
-// TODO remove this when code is in a more complete state
-#![allow(unused)]
-
-pub mod resource_provider;
-mod resources;
--- i18n/src/resource_provider.rs
+++
@@ -1,6 +0,0 @@
-use std::error::Error;
-
-#[async_trait::async_trait]
-pub trait ResourceProvider {
-	async fn resource_for(&mut self, resource: &str) -> Result<String, Box<dyn Error>>;
-}
