2068818235c35f2b755be2fb7b86ab02bcd71edf
Meadowsys <blazeykirin@gmail.com>
2023-12-20 20:39:10 -0800

    packid and git version handling

--- lib/src/datasource.rs
+++ lib/src/datasource.rs
@@ -1,11 +1,13 @@
 //! root manifest for a datasource (equivalent-ish to pack.mcmeta of a resource pack,
 //! i suppose)
 
-use camino::Utf8PathBuf;
+use camino::{ Utf8Path, Utf8PathBuf };
 use crate::error::*;
 use serde::{ Deserialize, Serialize };
+use std::process::Stdio;
 use tokio::fs;
 use tokio::io::AsyncReadExt;
+use tokio::process::Command;
 
 const SOURCE_META_FILENAME: &str = "pack.wiwimeta";
 
@@ -15,6 +17,7 @@ enum MetaFile {
 	#[serde(rename = "1")]
 	Version1 {
 		name: String,
+		packid: String,
 		description: Option<String>,
 		version: Option<String>
 	}
@@ -23,6 +26,7 @@ enum MetaFile {
 #[derive(Debug)]
 pub struct Source {
 	name: String,
+	packid: String,
 	description: Option<String>,
 	version: String
 }
@@ -30,34 +34,112 @@ pub struct Source {
 impl Source {
 	pub async fn new(dir: Utf8PathBuf) -> Result<Self> {
 		let _ = fs::read_dir(&dir).await
-			.map_err(|source| Error::SourceDirReadError { path: dir.clone(), source })?;
+			.map_err(|source| Error::SourceDirReadError { source, path: dir.clone() })?;
 
-		let mut manifest_path = dir;
+		let mut manifest_path = dir.clone();
 		manifest_path.push(SOURCE_META_FILENAME);
 
 		let mut manifest_reader = fs::OpenOptions::new()
 			.read(true)
 			.open(&manifest_path)
 			.await
-			.map_err(|source| Error::SourceManifestReadError { path: manifest_path.clone(), source })?;
+			.map_err(|source| Error::SourceManifestReadError { source, path: manifest_path.clone() })?;
 		let manifest_meta = fs::metadata(&manifest_path)
 			.await
-			.map_err(|source| Error::FileIOError { path: manifest_path.clone(), source })?;
+			.map_err(|source| Error::FileIOError { source, path: manifest_path.clone() })?;
 
 		let mut manifest_file = Vec::with_capacity(manifest_meta.len() as usize);
 		manifest_reader.read_to_end(&mut manifest_file)
 			.await
-			.map_err(|source| Error::FileIOError { path: manifest_path, source })?;
+			.map_err(|source| Error::FileIOError { source, path: manifest_path })?;
 
 		let manifest_file = String::from_utf8(manifest_file)?;
 
-		let (name, description, version) = match ron::from_str(&manifest_file)? {
-			MetaFile::Version1 { name, description, version } => {
-				let version = version.unwrap_or_else(|| "unknown".into());
-				(name, description, version)
+		let (name, packid, description, version) = match ron::from_str(&manifest_file)? {
+			MetaFile::Version1 { name, packid, description, version } => {
+				(name, packid, description, version)
 			}
 		};
 
-		Ok(Source { name, description, version })
+		let version = match version {
+			None => { "unknown".into() }
+			Some(version) => {
+				const GIT_HASH: &str = "git-hash";
+				const GIT_SHORT_HASH: &str = "git-short-hash";
+				const GIT_TAG: &str = "git-tag";
+
+				// TODO: make git executable configurable (from the GUI too)
+				match &*version {
+					GIT_HASH => {
+						git_rev_parse_head(&dir)
+							.await?
+					}
+					GIT_SHORT_HASH => {
+						let hash = git_rev_parse_head(&dir).await?;
+						hash[..10].into()
+					}
+					GIT_TAG => {
+						git_tag(&dir).await?
+					}
+					_ => { version }
+				}
+			}
+		};
+
+		Ok(Source { name, packid, description, version })
 	}
+}
+
+async fn git_rev_parse_head(dir: &Utf8Path) -> Result<String> {
+	const REV_PARSE_HEAD_CMD: &str = "git rev-parse HEAD";
+	const REV_PARSE_HEAD_CMD_ARGS: (&str, &[&str]) = ("git", &["rev-parse", "HEAD"]);
+
+	let child = Command::new(REV_PARSE_HEAD_CMD_ARGS.0)
+		.args(REV_PARSE_HEAD_CMD_ARGS.1)
+		.stdout(Stdio::piped())
+		.current_dir(dir)
+		.spawn()
+		.map_err(|source| Error::ChildProcessFailedToSpawnForGitVersioning {
+			source,
+			command: REV_PARSE_HEAD_CMD.into()
+		})?;
+	let output = child.wait_with_output()
+		.await
+		.map_err(|source| Error::ChildProcessFailedToSpawnForGitVersioning {
+			source,
+			command: REV_PARSE_HEAD_CMD.into()
+		})?;
+
+	let hash = String::from_utf8(output.stdout)?
+		.trim()
+		.into();
+
+	Ok(hash)
+}
+
+async fn git_tag(dir: &Utf8Path) -> Result<String> {
+	const TAG_CMD: &str = "git describe --tags --abbrev=10 --always";
+	const TAG_CMD_ARGS: (&str, &[&str]) = ("git", &["describe", "--tags", "--abbrev=10", "--always"]);
+
+	let child = Command::new(TAG_CMD_ARGS.0)
+		.args(TAG_CMD_ARGS.1)
+		.stdout(Stdio::piped())
+		.current_dir(dir)
+		.spawn()
+		.map_err(|source| Error::ChildProcessFailedToSpawnForGitVersioning {
+			source,
+			command: TAG_CMD.into()
+		})?;
+	let output = child.wait_with_output()
+		.await
+		.map_err(|source| Error::ChildProcessFailedToSpawnForGitVersioning {
+			source,
+			command: TAG_CMD.into()
+		})?;
+
+	let tag = String::from_utf8(output.stdout)?
+		.trim()
+		.into();
+
+	Ok(tag)
 }
--- lib/src/error.rs
+++ lib/src/error.rs
@@ -6,10 +6,22 @@ pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
+	#[error("failed to spawn child process using command \"{command}\":\n{source}")]
+	ChildProcessFailedToSpawn {
+		source: std::io::Error,
+		command: String
+	},
+
+	#[error("failed to spawn child process getting version information using command \"{command}\":\n{source}")]
+	ChildProcessFailedToSpawnForGitVersioning {
+		source: std::io::Error,
+		command: String
+	},
+
 	#[error("error doing IO on file at path \"{path}\":\n{source}")]
 	FileIOError {
-		path: Utf8PathBuf,
-		source: std::io::Error
+		source: std::io::Error,
+		path: Utf8PathBuf
 	},
 
 	#[error("invalid utf-8:\n{0}")]
@@ -17,14 +29,14 @@ pub enum Error {
 
 	#[error("error reading datasource manifest file at path \"{path}\":\n{source}")]
 	SourceManifestReadError {
-		path: Utf8PathBuf,
-		source: std::io::Error
+		source: std::io::Error,
+		path: Utf8PathBuf
 	},
 
 	#[error("error reading source directory at path \"{path}\":\n{source}")]
 	SourceDirReadError{
-		path: Utf8PathBuf,
-		source: std::io::Error
+		source: std::io::Error,
+		path: Utf8PathBuf
 	},
 
 	#[error("error parsing ron:\n{source}")]
