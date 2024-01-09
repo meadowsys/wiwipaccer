f2eb84b5b93e5e85ba173209d3bed1367c6cf65a
Meadowsys <blazeykirin@gmail.com>
2024-01-08 21:21:58 -0800

    a window shows up aha

--- packages/app/src-tauri/src/main.rs
+++ packages/app/src-tauri/src/main.rs
@@ -3,8 +3,30 @@
 	windows_subsystem = "windows"
 )]
 
+use ::tauri::{ async_runtime, WindowBuilder, WindowUrl };
+
 fn main() {
+	let rt = tokio::runtime::Builder::new_multi_thread()
+		.enable_all()
+		.worker_threads(2)
+		.build()
+		.expect("error building async runtime");
+	async_runtime::set(rt.handle().clone());
+
 	tauri::Builder::default()
+		.setup(|app| {
+			let handle = app.handle();
+
+			let start_url = WindowUrl::App("start".into());
+			let _window = WindowBuilder::new(handle, "start", start_url)
+				.accept_first_mouse(false)
+				.enable_clipboard_access()
+				.title("")
+				.build()
+				.expect("failed to create window");
+
+			Ok(())
+		})
 		.invoke_handler(tauri::generate_handler![])
 		.build(tauri::generate_context!())
 		.expect("error running app")
