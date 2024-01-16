c42755d55ab0d86259aff7329555570b382b4b31
Meadowsys <blazeykirin@gmail.com>
2024-01-15 18:38:00 -0800

    impl serialise/deserialise for the stuff

--- packages/core/src/mc_versions.rs
+++ packages/core/src/mc_versions.rs
@@ -1,7 +1,10 @@
+use ::serde::{ Deserialize, Deserializer, Serialize, Serializer };
+
 pub struct MCVersion {
 	inner: Inner
 }
 
+#[derive(Deserialize, Serialize)]
 pub struct Inner {
 	pub name: &'static str,
 	pub release_type: ReleaseType,
@@ -14,17 +17,27 @@ pub struct Inner {
 	pub n: usize
 }
 
+#[derive(Deserialize, Serialize)]
 pub enum ReleaseType {
+	#[serde(rename = "snapshot")]
 	Snapshot,
+	#[serde(rename = "release")]
 	Release,
+	#[serde(rename = "old_beta")]
 	OldBeta,
+	#[serde(rename = "old_alpha")]
 	OldAlpha
 }
 
+#[derive(Deserialize, Serialize)]
 pub enum PackFormat {
+	#[serde(rename = "verified")]
 	Verified(u8),
+	#[serde(rename = "unverified")]
 	Unverified(u8),
+	#[serde(rename = "unknown")]
 	Unknown,
+	#[serde(rename = "none")]
 	None
 }
 
@@ -32,6 +45,25 @@ impl ::std::ops::Deref for MCVersion {
 	type Target = Inner;
 	fn deref(&self) -> &Self::Target {
 		&self.inner
+	}
+}
+
+impl Serialize for MCVersion {
+	fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+	where
+		S: Serializer
+	{
+		<Inner as Serialize>::serialize(&self.inner, serializer)
+	}
+}
+
+impl Deserialize<'static> for MCVersion {
+	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+	where
+		D: Deserializer<'static>
+	{
+		let inner = <Inner as Deserialize>::deserialize(deserializer)?;
+		Ok(Self { inner })
 	}
 }
 
