a648e45d1cdf512c29b9a2f72342febd8b7acafa
Meadowsys <blazeykirin@gmail.com>
2024-01-19 03:38:58 -0800

    but maybe i shelf it a bit and actually build something first, then see what i can do with it

--- packages/app/uno.config.ts
+++ packages/app/uno.config.ts
@@ -5,12 +5,12 @@ export default defineConfig({
 	presets: [
 		preset_mini()
 	],
-	theme: {
-		colors: {
-			primary: "var(--wiwi-primary)",
-			secondary: "var(--wiwi-secondary)",
-			background: "var(--wiwi-background)",
-			text: "var(--wiwi-text)"
-		}
-	}
+	// theme: {
+	// 	colors: {
+	// 		primary: "var(--wiwi-primary)",
+	// 		secondary: "var(--wiwi-secondary)",
+	// 		background: "var(--wiwi-background)",
+	// 		text: "var(--wiwi-text)"
+	// 	}
+	// }
 });
