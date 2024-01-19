e709a5fe4b71bf4c15ec75625f9f7381512a1948
Meadowsys <blazeykirin@gmail.com>
2024-01-19 03:38:20 -0800

    tweak?

--- packages/app/stores/theme.ts
+++ packages/app/stores/theme.ts
@@ -1,27 +1,29 @@
 import { defu } from "defu";
 
+export type ResolvedTheme = {
+	[k in string]?: string;
+};
+
 export type Theme = {
 	extends?: Array<Theme>;
-} & {
-	[k in string]?: string;
-}
+} & ResolvedTheme
 
 export const use_theme = defineStore("theme", () => {
 	const loaded_theme = ref<Theme>({});
-	const resolved_theme = computed<Theme>(() => resolve_theme(loaded_theme.value));
+	const resolved_theme = computed<ResolvedTheme>(() => resolve_theme(loaded_theme.value));
 
-	function set_theme(theme: Theme) {
+	function _set_theme(theme: Theme) {
 		loaded_theme.value = theme;
 	}
 
 	return {
 		theme: resolved_theme,
-		set_theme
+		_set_theme
 	};
 });
 
 function resolve_theme(theme: Theme) {
-	const resolved: Omit<Theme, "extends"> = {};
+	const resolved: ResolvedTheme = {};
 
 	Object.entries(theme)
 		.filter(k => k[0] !== "extends")
