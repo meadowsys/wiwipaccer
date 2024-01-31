2171a3dbe573ab1350f88bb5688164b3c730c5eb
Meadowsys <blazeykirin@gmail.com>
2024-01-31 00:06:47 -0800

    oop, comment
    uwu

--- packages/app/pages/workspace.vue
+++ packages/app/pages/workspace.vue
@@ -10,7 +10,4 @@
 	let name = (await invoke_get_workspace_name())!;
 	let mc_version = "1.18.2";
 	let stuff = JSON.stringify(await invoke_get_frontend_data_for({ name, mc_version }), null, "   ");
-	// invoke_get_frontend_data_for({
-	// 	name: "uwu",
-	// })
 </script>
