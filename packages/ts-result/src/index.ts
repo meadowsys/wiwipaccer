68d325cdcecb731ba3803fe28b28d9f885584bbc
Meadowsys <blazeykirin@gmail.com>
2024-02-08 01:38:54 -0800

    whoopsie

--- packages/ts-result/src/index.ts
+++ packages/ts-result/src/index.ts
@@ -1,10 +1,5 @@
 import { z, type ZodType } from "zod";
 
-export type TSResultParam<T, E> = {
-	ok: T;
-	err: E;
-}
-
 export function ts_result<T extends ZodType>(value: T) {
 	return z.union([
 		z.object({
