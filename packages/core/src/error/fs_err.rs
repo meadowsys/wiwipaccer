9a7a52093fb60f7b9b6e627589831e5b0f3fd488
Meadowsys <blazeykirin@gmail.com>
2024-02-09 00:01:10 -0800

    some more error ports

--- packages/core/src/error/fs_err.rs
+++ packages/core/src/error/fs_err.rs
@@ -13,96 +13,100 @@ impl NiceErrorMessage for SpawnBlocking {
 	}
 }
 
-pub enum MetadataWithoutPath {
+pub struct WithPath<E> {
+	error: E,
+	path: String
+}
+
+impl<E: NiceErrorMessage> NiceErrorMessage for WithPath<E> {
+	fn fmt(&self, f: &mut Formatter) {
+		f.write_str("for path: ");
+		f.write_str(&self.path);
+		f.next_line();
+
+		f.with_indent(|f| {
+			f.fmt(&self.error);
+		});
+	}
+}
+
+pub enum Metadata {
 	FileSystem {
 		error: ::std::io::Error
 	},
-	JoinError {
+	Join {
 		error: SpawnBlocking
 	}
 }
 
-impl MetadataWithoutPath {
-	pub fn with_path(self, path: String) -> Metadata {
-		let error = self;
-		Metadata { error, path }
+pub type MetadataWithPath = WithPath<Metadata>;
+
+impl Metadata {
+	pub fn with_path(self, path: String) -> WithPath<Self> {
+		WithPath { error: self, path }
 	}
 }
 
-impl NiceErrorMessage for MetadataWithoutPath {
+impl NiceErrorMessage for Metadata {
 	fn fmt(&self, f: &mut Formatter) {
 		f.write_args(format_args!("error fetching metadata"));
 
-		use MetadataWithoutPath::*;
-		f.with_indent(|f| {
-			match self {
-				FileSystem { error } => {
-					f.write_args(format_args!("{error}"));
-				}
-				JoinError { error } => {
-					f.fmt(error);
-				}
+		use Metadata::*;
+		f.with_indent(|f| match self {
+			FileSystem { error } => {
+				f.write_args(format_args!("{error}"));
+			}
+			Join { error } => {
+				f.fmt(error);
 			}
 		});
 	}
 }
 
-pub struct Metadata {
-	error: MetadataWithoutPath,
-	path: String
-}
-
-
-impl NiceErrorMessage for Metadata {
-	fn fmt(&self, f: &mut Formatter) {
-		f.write_line_args(format_args!("for path {}", self.path));
-		f.fmt(&self.error);
-	}
-}
-
 pub struct IsDir {
-	error: Metadata,
-	path: String
+	error: WithPath<Metadata>
 }
 
 impl NiceErrorMessage for IsDir {
 	fn fmt(&self, f: &mut Formatter) {
 		f.write_line_args(format_args!("error checking if path is a dir"));
 		f.with_indent(|f| {
-			f.write_str("path: ");
-			f.write_str(&self.path);
-			f.next_line();
 			f.fmt(&self.error);
 		})
 	}
 }
 
 pub struct IsFile {
-	error: Metadata,
-	path: String
+	error: WithPath<Metadata>
 }
 
 impl NiceErrorMessage for IsFile {
 	fn fmt(&self, f: &mut Formatter) {
 		f.write_line_args(format_args!("error checking if path is a file"));
 		f.with_indent(|f| {
-			f.write_str("path: ");
-			f.write_str(&self.path);
-			f.next_line();
 			f.fmt(&self.error);
 		})
 	}
 }
 
-// pub enum ReadToString {
-// 	Read {
-// 		error: ::std::io::Error
-// 	},
-// 	Utf8 {
-// 		error: ::std::str::Utf8Error,
-// 		bytes: Vec<u8>
-// 	}
-// }
+pub enum ReadToString {
+	FileSystem {
+		error: ::std::io::Error
+	},
+	Join {
+		error: SpawnBlocking
+	},
+	UTF8 {
+		error: ::std::str::Utf8Error,
+		bytes: Vec<u8>
+	}
+}
+
+impl NiceErrorMessage for ReadToString {
+	fn fmt(&self, f: &mut Formatter) {
+		todo!()
+	}
+}
 
 // pub struct ReadDir {
 // 	error: ::std::io::Error
@@ -114,44 +118,34 @@ pub fn spawn_blocking(error: ::tokio::task::JoinError) -> SpawnBlocking {
 	SpawnBlocking { error }
 }
 
-pub fn metadata_fs<F>(path_fn: F)
-	-> impl FnOnce(::std::io::Error) -> Metadata
-where
-	F: FnOnce() -> String
+pub fn metadata_fs(error: ::std::io::Error)
+	-> Metadata
 {
-	|error| Metadata {
-		error: MetadataWithoutPath::FileSystem { error },
-		path: path_fn()
-	}
+	Metadata::FileSystem { error }
 }
 
-pub fn metadata_join<F>(path_fn: F)
-	-> impl FnOnce(SpawnBlocking) -> Metadata
-where
-	F: FnOnce() -> String
+pub fn metadata_join(error: SpawnBlocking)
+	-> Metadata
 {
-	|error| Metadata {
-		error: MetadataWithoutPath::JoinError { error },
-		path: path_fn()
-	}
+	Metadata::Join { error }
 }
 
-pub fn metadata_without_path_fs(error: ::std::io::Error)
-	-> MetadataWithoutPath
-{
-	MetadataWithoutPath::FileSystem { error }
+pub fn is_dir(error: WithPath<Metadata>) -> IsDir {
+	IsDir { error }
 }
 
-pub fn metadata_without_path_join(error: SpawnBlocking)
-	-> MetadataWithoutPath
-{
-	MetadataWithoutPath::JoinError { error }
+pub fn is_file(error: WithPath<Metadata>) -> IsFile {
+	IsFile { error }
 }
 
-pub fn is_dir(error: Metadata, path: String) -> IsDir {
-	IsDir { error, path }
+pub fn read_to_string_fs(error: ::std::io::Error) -> ReadToString {
+	ReadToString::FileSystem { error }
 }
 
-pub fn is_file(error: Metadata, path: String) -> IsFile {
-	IsFile { error, path }
+pub fn read_to_string_join(error: SpawnBlocking) -> ReadToString {
+	ReadToString::Join { error }
+}
+
+pub fn read_to_string_utf8(error: ::std::str::Utf8Error, bytes: Vec<u8>) -> ReadToString {
+	ReadToString::UTF8 { error, bytes }
 }
--- packages/core/src/util/fs2.rs
+++ packages/core/src/util/fs2.rs
@@ -3,67 +3,54 @@ use crate::nom as n;
 use ::std::fs;
 use ::std::io::Read as _;
 
-// pub async fn metadata<F>(path_fn: F) -> Result<fs::Metadata, fs_err::Metadata>
-// where
-// 	F: Fn() -> String + Clone + Send + 'static
-// {
-// 	let path = path_fn();
-// 	let _path_fn = path_fn.clone();
-// 	spawn_blocking(
-// 		|| fs::metadata(path).map_err(fs_err::metadata_fs_err(_path_fn)),
-// 		fs_err::metadata_join_err(path_fn)
-// 	).await
-// }
-pub async fn metadata<F>(path_fn: F) -> Result<fs::Metadata, fs_err::Metadata>
+pub async fn metadata<F>(path_fn: F) -> Result<fs::Metadata, fs_err::MetadataWithPath>
 where
-	F: Fn() -> String + Send
+	F: Fn() -> String
 {
 	let path = path_fn();
 	spawn_blocking(
-		|| fs::metadata(path).map_err(fs_err::metadata_without_path_fs),
-		fs_err::metadata_without_path_join
+		|| fs::metadata(path).map_err(fs_err::metadata_fs),
+		fs_err::metadata_join
 	).await.map_err(|e| e.with_path(path_fn()))
 }
 
 #[inline]
 pub async fn is_dir<F>(path_fn: F) -> Result<bool, fs_err::IsDir>
 where
-	F: Fn() -> String + Send + Clone
+	F: Fn() -> String
 {
-	metadata(path_fn.clone()).await
+	metadata(path_fn).await
 		.map(|m| m.is_dir())
-		.map_err(|e| fs_err::is_dir(e, path_fn()))
-	// Ok(metadata(path_fn).await?.is_dir())
+		.map_err(fs_err::is_dir)
 }
 
 
 #[inline]
 pub async fn is_file2<F>(path_fn: F) -> Result<bool, fs_err::IsFile>
 where
-	F: Fn() -> String + Send + Clone
+	F: Fn() -> String
 {
-	metadata(path_fn.clone()).await
+	metadata(path_fn).await
 		.map(|m| m.is_file())
-		.map_err(|e| fs_err::is_file(e, path_fn()))
-	// Ok(metadata(path_fn).await?.is_file())
+		.map_err(fs_err::is_file)
 }
 
-// #[inline]
-// pub async fn read_to_string(path: n::global::FilePath) -> Result<String> {
-// 	read_to_string2(path.into_inner()).await
-// }
+// TODO: can probably be optimised (one less meta call?) if rewritten by hand?
+pub async fn read_to_string<F>(path_fn: F) -> Result<String, fs_err::ReadToString>
+where
+	F: Fn() -> String
+{
+	let path = path_fn();
+	let bytes = spawn_blocking(
+		|| fs::read(path).map_err(fs_err::read_to_string_fs),
+		fs_err::read_to_string_join
+	).await?;
 
-// // TODO: can probably be optimised (one less meta call?) if rewritten by hand?
-// pub async fn read_to_string2(path: String) -> Result<String> {
-// 	let f = || fs::read(path)
-// 		.map_err(Error::FSError);
-// 	let bytes = spawn_blocking(f).await?;
-
-// 	match std::str::from_utf8(&bytes) {
-// 		Ok(_) => { Ok(unsafe { String::from_utf8_unchecked(bytes) }) }
-// 		Err(source) => { Err(Error::Utf8Error { source, bytes }) }
-// 	}
-// }
+	match std::str::from_utf8(&bytes) {
+		Ok(_) => { Ok(unsafe { String::from_utf8_unchecked(bytes) }) }
+		Err(error) => { Err(fs_err::read_to_string_utf8(error, bytes)) }
+	}
+}
 
 // #[inline]
 // pub async fn read_dir(path: n::global::DirPath) -> Result<ReadDir> {
