d06c6dfd84f147c63f36761b07ff78728389c609
Meadowsys <blazeykirin@gmail.com>
2024-02-20 10:59:11 -0800

    weird roundabout way to print nicer messages lol
    now it doesn't print the header of "MC versions without a declared pack format:" until its needed, and use whether or not that (zero size, lol) closure was initialised to decide whether or not to print "none" at the end, essentially
    
    could have been done simpler with a bool variable lel

--- packages/mc-versions/src/bin/list-unknown-versions.rs
+++ packages/mc-versions/src/bin/list-unknown-versions.rs
@@ -1,11 +1,19 @@
+use ::lazy_wrap::LazyWrap;
 use ::mc_versions::{ MC_VERSIONS, PackFormat::Unknown };
 
 fn main() {
-	println!("MC versions without a declared pack format:");
+	let printer = LazyWrap::new(|| {
+		println!("MC versions without a declared pack format:");
+		|v| println!("   {v}")
+	});
 
 	MC_VERSIONS
 		.iter()
 		.filter(|v| matches!(v.pack_format, Unknown))
 		.map(|v| v.name)
-		.for_each(|v| println!("   {v}"));
+		.for_each(|v| (*printer)(v));
+
+	if !LazyWrap::is_initialised(&printer) {
+		println!("everything declared!");
+	}
 }
