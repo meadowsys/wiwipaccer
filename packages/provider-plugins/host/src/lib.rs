cf58b25dcf002bb72d211c6cd189a899e1c4a9da
Meadow Liu <meadowsys@kiwin.gay>
2024-11-03 00:44:50 -0700

    try a wasm component thing
    haven't tried to create a host / run it yet, at time of this commit

---
+++ packages/provider-plugins/host/src/lib.rs
@@ -0,0 +1,28 @@
+// #[repr(C)]
+// pub struct PluginMetadata {
+// 	pub name: &'static str,
+// 	pub id: &'static str,
+// 	pub version: &'static str,
+// 	pub authors: &'static [&'static str],
+// 	pub properties: &'static [PluginProperty]
+// }
+
+// #[repr(C)]
+// pub struct PluginProperty {
+// 	pub required: bool,
+// 	pub key: &'static str,
+// 	pub val_type: PluginPropertyType
+// }
+
+// #[repr(C)]
+// pub enum PluginPropertyType {
+// 	Int,
+// 	Float,
+// 	String,
+// 	Array(&'static PluginPropertyType),
+// 	Map(&'static [PluginProperty])
+// }
+
+pub fn å() -> &'static str {
+	"aaaaaaaaaaaaaaaaaa"
+}
