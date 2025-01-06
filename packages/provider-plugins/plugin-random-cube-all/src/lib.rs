364ad0051e7ddf4b6a450afd2f37dbfafbb11efb
Meadow Liu <meadowsys@kiwin.gay>
2025-01-06 01:58:51 -0800

    upgrade rust toolchain and make a few tweaks for now

--- packages/provider-plugins/plugin-random-cube-all/src/lib.rs
+++ packages/provider-plugins/plugin-random-cube-all/src/lib.rs
@@ -1,24 +1,28 @@
-wit_bindgen::generate! {{
-	path: "../plugin.wit",
-	ownership: Borrowing { duplicate_if_necessary: true }
-}}
+// wit_bindgen::generate! {{
+// 	path: "../plugin.wit",
+// 	ownership: Borrowing { duplicate_if_necessary: true }
+// }}
 
-use exports::wiwi::wiwipaccer_provider_plugin::imp;
-use wiwi::wiwipaccer_provider_plugin::api;
+// use exports::wiwi::wiwipaccer_provider_plugin::imp;
+// use wiwi::wiwipaccer_provider_plugin::api;
 
-struct Plugin;
-export!(Plugin);
+// struct Plugin;
+// export!(Plugin);
 
-impl imp::Guest for Plugin {
-	fn metadata() -> imp::PluginMetadata {
-		imp::PluginMetadata {
-			ns: "wiwi".into(),
-			id: "random-cube-all".into(),
-			version: "0.1.0-testlol".into(),
-			authors: vec![
-				"Meadow Liu <meadowsys@kiwin.gay>".into(),
-				api::h()
-			]
-		}
-	}
+// impl imp::Guest for Plugin {
+// 	fn metadata() -> imp::PluginMetadata {
+// 		imp::PluginMetadata {
+// 			ns: "wiwi".into(),
+// 			id: "random-cube-all".into(),
+// 			version: "0.1.0-testlol".into(),
+// 			authors: vec![
+// 				"Meadow Liu <meadowsys@kiwin.gay>".into(),
+// 				api::h()
+// 			]
+// 		}
+// 	}
+// }
+
+pub const fn å() -> &'static str {
+	"aaaaaaaaaaaaaaaaaa"
 }
