117bca9cdac5c41b23421f2731939934c26deede
Meadow Liu <meadowsys@kiwin.gay>
2025-03-14 23:34:50 -0700

    experiment with host or something idk

--- packages/provider-plugins/host/src/lib.rs
+++ packages/provider-plugins/host/src/lib.rs
@@ -1,28 +1,29 @@
-// #[repr(C)]
-// pub struct PluginMetadata {
-// 	pub name: &'static str,
-// 	pub id: &'static str,
-// 	pub version: &'static str,
-// 	pub authors: &'static [&'static str],
-// 	pub properties: &'static [PluginProperty]
-// }
+wasmtime::component::bindgen!({
+	path: "../plugin.wit",
 
-// #[repr(C)]
-// pub struct PluginProperty {
-// 	pub required: bool,
-// 	pub key: &'static str,
-// 	pub val_type: PluginPropertyType
-// }
+	// ???
+	// async: true,
+	// or configure it more finely idk
+	ownership: Borrowing {
+		duplicate_if_necessary: true
+	}
+});
 
-// #[repr(C)]
-// pub enum PluginPropertyType {
-// 	Int,
-// 	Float,
-// 	String,
-// 	Array(&'static PluginPropertyType),
-// 	Map(&'static [PluginProperty])
-// }
+fn _test_todo_remove_me() {
+	use wasmtime::component::{ Component, Linker };
+	use wasmtime::{ Engine, Store };
 
-pub const fn å() -> &'static str {
-	"aaaaaaaaaaaaaaaaaa"
+	struct State;
+
+	impl self::wiwi::wiwipaccer_provider_plugin::api::Host for State {}
+	impl self::wiwi::wiwipaccer_provider_plugin::types::Host for State {}
+
+	let engine = Engine::default();
+	let component = Component::from_binary(&engine, b"Uwuwuwuwuuwuuwuuwuwuwuwuwu").unwrap();
+
+	let mut linker = Linker::new(&engine);
+	Plugin::add_to_linker::<State, _>(&mut linker, |state| state).unwrap();
+
+	let mut store = Store::new(&engine, State);
+	let _bindings = Plugin::instantiate(&mut store, &component, &linker).unwrap();
 }
