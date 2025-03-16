203b9c28167ae1c2fa379dddfa383fcc0e924e99
Meadow Liu <meadowsys@kiwin.gay>
2025-03-15 23:19:11 -0700

    try to set up a frontend project

---
+++ packages/app-frontend/src/lib.rs
@@ -0,0 +1,15 @@
+#![feature(panic_update_hook)]
+
+use leptos::prelude::*;
+use std::panic;
+use wasm_bindgen::prelude::wasm_bindgen;
+
+#[wasm_bindgen(start)]
+fn main() {
+	panic::update_hook(|prev_hook, info| {
+		console_error_panic_hook::hook(info);
+		prev_hook(info);
+	});
+
+	mount_to_body(|| "uwuwuwuwuwu");
+}
