28f1b4ba7607a41f0ff2c78b9c95dbf770e0d0a8
Meadow Liu <meadowsys@kiwin.gay>
2025-03-16 00:32:02 -0700

    make window appear (and verify it loads, builds, and packaged build runs)

--- packages/app-desktop/src/main.rs
+++ packages/app-desktop/src/main.rs
@@ -4,9 +4,18 @@
 	windows_subsystem = "windows"
 )]
 
+use tauri::{ WebviewWindowBuilder, WebviewUrl };
+
 fn main() {
 	tauri::Builder::<tauri::Wry>::new()
 		.invoke_handler(tauri::generate_handler![])
+		.setup(|app| {
+			WebviewWindowBuilder::new(app.handle(), "main", WebviewUrl::App("".into()))
+				.accept_first_mouse(cfg!(target_os = "macos"))
+				.build()
+				.unwrap();
+			Ok(())
+		})
 		.build(tauri::generate_context!())
 		.expect("error while running app")
 		.run(|_app_handle, _event| {
